a1418e7c355fe50eff2cb80062a03a530c6bbf97
2300033279 <163741962+2300033279@users.noreply.github.com>
2026-01-30 15:15:19 +0530

    Replace TemperatureCheck with HelloWorld class
    Replaced TemperatureCheck class with HelloWorld class and updated main method to print a message.

--- sample.java
+++ sample.java
@@ -1,22 +1,5 @@
-import java.util.Scanner;
-
-public class TemperatureCheck {
+public class HelloWorld {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter temperature: ");
-        int temp = sc.nextInt();
-
-        if (temp < 0) {
-            System.out.println("Freezing weather");
-        } else if (temp < 20) {
-            System.out.println("Cold weather");
-        } else if (temp < 30) {
-            System.out.println("Normal weather");
-        } else {
-            System.out.println("Hot weather");
-        }
-
-        sc.close();
+        System.out.println("Hello from Jenkins Pipeline!");
     }
 }
